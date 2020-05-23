ee473af5b1a205f2cac293520647ba101c0f3e61
mgrohs <marc.grohs@gmail.com>
2020-05-23 02:56:50 +0200

    deprecated .getAttribute added new site

--- mangaArrows.js
+++ mangaArrows.js
@@ -3,31 +3,27 @@
 const baseURL = window.location.origin;
 let prevURL, nextURL;
 
-if (baseURL.includes('leviatanscans') || baseURL.includes('zeroscans')) {
-    prevURL = document
-        .querySelector('.fa-arrow-left')
-        .parentElement.getAttribute('href');
-    nextURL = document
-        .querySelector('.fa-arrow-right')
-        .parentElement.getAttribute('href');
+if (
+    baseURL.includes('leviatanscans') ||
+    baseURL.includes('zeroscans') ||
+    baseURL.includes('reaperscans')
+) {
+    prevURL = document.querySelector('.fa-arrow-left').parentElement.href;
+    nextURL = document.querySelector('.fa-arrow-right').parentElement.href;
 } else if (baseURL.includes('manganelo')) {
-    prevURL = document
-        .querySelector('.navi-change-chapter-btn-prev')
-        .getAttribute('href');
-    nextURL = document
-        .querySelector('.navi-change-chapter-btn-next')
-        .getAttribute('href');
+    prevURL = document.querySelector('.navi-change-chapter-btn-prev').href;
+    nextURL = document.querySelector('.navi-change-chapter-btn-next').href;
 } else if (baseURL.includes('webtoon')) {
-    prevURL = document.querySelector('.pg_prev').getAttribute('href');
-    nextURL = document.querySelector('.pg_next').getAttribute('href');
+    prevURL = document.querySelector('.pg_prev').href;
+    nextURL = document.querySelector('.pg_next').href;
 }
 
 document.addEventListener('keydown', (e) => {
-    // go previous chapter
+    // go previous chapter with left arrow
     if (e.keyCode === 37) {
         window.location.href = prevURL;
     }
-    // go next chapter
+    // go next chapter with right arrow
     if (e.keyCode === 39) {
         window.location.href = nextURL;
     }
