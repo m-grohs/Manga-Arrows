ad905c957d2823bf9eb9f9b8c8215996d0fd7e8e
mgrohs <marc.grohs@gmail.com>
2020-06-13 19:11:24 +0200

    refactor to incorporate generic themes

--- mangaArrows.js
+++ mangaArrows.js
@@ -1,34 +1,34 @@
 'use strict';
 
-const baseURL = window.location.origin;
+const BASE_URL = window.location.origin;
+const BASE_PATH = window.location.pathname;
 let prevSrc, nextSrc;
 
 // Get the Source Class of the Prev/Next Button
-if (
-    baseURL.includes('leviatanscans') ||
-    baseURL.includes('zeroscans') ||
-    baseURL.includes('reaperscans') ||
-    baseURL.includes('methodscans')
-) {
+// BASE_URL for unique sites, BASE_PATH for generic themes sites
+if (BASE_PATH.includes('/comics/')) {
     prevSrc = '.fa-arrow-left';
     nextSrc = '.fa-arrow-right';
-} else if (baseURL.includes('manganelo')) {
+} else if (BASE_URL.includes('manganelo')) {
     prevSrc = '.navi-change-chapter-btn-prev';
     nextSrc = '.navi-change-chapter-btn-next';
-} else if (baseURL.includes('webtoon')) {
+} else if (BASE_URL.includes('webtoon')) {
     prevSrc = '.pg_prev';
     nextSrc = '.pg_next';
 }
 
-document.addEventListener('keydown', (e) => {
-    // go previous chapter with left arrow
-    if (e.keyCode === 37) {
-        window.location.href =
-            document.querySelector(prevSrc).href || document.querySelector(prevSrc).parentElement.href;
-    }
-    // go next chapter with right arrow
-    if (e.keyCode === 39) {
-        window.location.href =
-            document.querySelector(nextSrc).href || document.querySelector(nextSrc).parentElement.href;
-    }
-});
+// Check if Vars are set and process left/right arrow
+if (prevSrc && nextSrc) {
+    document.addEventListener('keydown', (e) => {
+        // go previous chapter with left arrow
+        if (e.keyCode === 37) {
+            window.location.href =
+                document.querySelector(prevSrc).href || document.querySelector(prevSrc).parentElement.href;
+        }
+        // go next chapter with right arrow
+        if (e.keyCode === 39) {
+            window.location.href =
+                document.querySelector(nextSrc).href || document.querySelector(nextSrc).parentElement.href;
+        }
+    });
+}
